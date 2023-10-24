dd73f6e1b0ddc81aa0bbb75645a0df90f24fdf63
김주환 <kjh71047@naver.com>
2023-10-25 03:17:25 +0900

    feat: 프로젝트 기본생성 및 깃헙액션 테스트

---
+++ chobang/src/main/java/com/chobang/ChobangApplication.java
@@ -0,0 +1,13 @@
+package com.chobang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChobangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChobangApplication.class, args);
+	}
+
+}
---
+++ chobang/src/main/java/com/chobang/config/CorsFilterConfig.java
@@ -0,0 +1,20 @@
+package com.chobang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsFilterConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+                .exposedHeaders("X-API-VERSION")
+                .allowedOriginPatterns("*:*")
+                .allowedHeaders("*");
+    }
+
+}
