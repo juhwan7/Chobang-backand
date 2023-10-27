aa0602817471b8a4ae02e467c8abc18f50107942
김주환 <kjh71047@naver.com>
2023-10-27 17:48:15 +0900

    feat: swagger 설정 추가

--- src/main/java/com/chobang/config/SwaggerConfig.java
+++ src/main/java/com/chobang/config/SwaggerConfig.java
@@ -3,18 +3,25 @@ package com.chobang.config;
 import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig  {
+@SuppressWarnings("unchecked")	// 무점검 경고 제거
+public class SwaggerConfig {
 
     @Bean
     public Docket api(TypeResolver typeResolver) {
@@ -24,10 +31,12 @@ public class SwaggerConfig  {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.chobang.controller"))
                 .paths(PathSelectors.any())
-                .build();
-//                .consumes(Collections.singleton(MediaType.MULTIPART_FORM_DATA_VALUE))
-//                .produces(Collections.singleton(MediaType.APPLICATION_JSON_VALUE));
-//                .ignoredParameterTypes(CustomUserDetail.class);
+                .build()
+                .globalResponseMessage(RequestMethod.GET, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.POST, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.DELETE, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.PUT, getCustomizedResponseMessages())
+                .globalResponseMessage(RequestMethod.PATCH, getCustomizedResponseMessages());
     }
 
     private ApiInfo apiInfo() {
@@ -38,4 +47,15 @@ public class SwaggerConfig  {
                 .build();
     }
 
+    private List<ResponseMessage> getCustomizedResponseMessages() {
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder().code(200).message("성공").build());
+        responseMessages.add(new ResponseMessageBuilder().code(204).message("데이터 미존재").build());
+        responseMessages.add(new ResponseMessageBuilder().code(400).message("입력값 오류").build());
+        responseMessages.add(new ResponseMessageBuilder().code(401).message("미 로그인").build());
+        responseMessages.add(new ResponseMessageBuilder().code(403).message("권한없음").build());
+        responseMessages.add(new ResponseMessageBuilder().code(412).message("처리중 오류").build());
+        responseMessages.add(new ResponseMessageBuilder().code(500).message("서버에러").build());
+        return responseMessages;
+    }
 }
